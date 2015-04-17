75f64867ae93d4a75db6ea874498a01ee2be0ce3
bladechapman <bechapm2@illinois.edu>
2015-04-16 23:53:21 -0500

    crude top_stories

--- HackerNews/DataStore.swift
+++ HackerNews/DataStore.swift
@@ -13,10 +13,10 @@ var sharedDataStore : DataStore?
 class DataStore: NSObject {
     
     let ref = Firebase(url: "https://hacker-news.firebaseio.com/v0/")
-    var items : AnyObject?
-    var newStories : AnyObject?
-    var topStories : AnyObject?
-    var users : AnyObject?
+    var items : FDataSnapshot?
+    var newStories : FDataSnapshot?
+    var topStories : FDataSnapshot?
+    var users : FDataSnapshot?
     
     override init() {
         
@@ -24,23 +24,30 @@ class DataStore: NSObject {
     
     class var sharedInstance : DataStore {
         if (sharedDataStore == nil) {
+            println("datastore initialized")
             sharedDataStore = DataStore()
         }
-        
         return sharedDataStore!
     }
     
-    func retrieveX(#endpoint: String, callback : (AnyObject? -> Void)?) {
+    func retrieveX(#endpoint: String, callback : (FDataSnapshot! -> Void)?) {
         self.ref.childByAppendingPath(endpoint).observeSingleEventOfType(.Value) { (snapshot : FDataSnapshot!) -> Void in
             if let unwrappedCallback = callback {
-                unwrappedCallback(snapshot.value)
+                unwrappedCallback(snapshot)
             }
         }
     }
-    func retrieveAllItems(#callback : (AnyObject? -> Void)?) { self.retrieveX(endpoint: "item", callback: callback)}
-    func retrieveNewStories(#callback : (AnyObject? -> Void)?) { self.retrieveX(endpoint: "newstories", callback: callback)}
-    func retrieveTopStories(#callback : (AnyObject? -> Void)?) { self.retrieveX(endpoint: "topstories", callback: callback)}
-    func retrieveUsers(#callback : (AnyObject? -> Void)?) { self.retrieveX(endpoint: "user", callback: callback)}
+    func retrieveAllItems(#callback : (FDataSnapshot! -> Void)?) { self.retrieveX(endpoint: "item", callback: callback)}
+    func retrieveNewStories(#callback : (FDataSnapshot! -> Void)?) { self.retrieveX(endpoint: "newstories", callback: callback)}
+    func retrieveTopStories(#callback : (FDataSnapshot! -> Void)?) { self.retrieveX(endpoint: "topstories", callback: callback)}
+    func retrieveUsers(#callback : (FDataSnapshot! -> Void)?) { self.retrieveX(endpoint: "user", callback: callback)}
+    func retrieveItemWithId(id : String, callback : (FDataSnapshot! -> Void)?) { self.retrieveX(endpoint: "item/\(id)", callback: callback)}
+    func retrieveUserWithId(id : String, callback : (FDataSnapshot! -> Void)?) { self.retrieveX(endpoint: "user/\(id)", callback: callback)}
     
-    
+    func purge() {
+        items = nil
+        newStories = nil
+        topStories = nil
+        users = nil
+    }
 }
--- HackerNews/ViewController.swift
+++ HackerNews/ViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var newsTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         var store = DataStore.sharedInstance
-        store.retrieveTopStories() { (data : AnyObject?) -> Void in
-            println(data)
-        }
+        store.retrieveTopStories(callback: { (data : FDataSnapshot!) -> Void in
+            store.topStories = data
+            self.newsTable.reloadData()
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,6 +28,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var store = DataStore.sharedInstance
+        
+        var cell = newsTable.dequeueReusableCellWithIdentifier("itemCell") as! UITableViewCell
+        if let label : AnyObject = store.topStories?.value[indexPath.row] {
+            store.retrieveItemWithId(String(stringInterpolationSegment: store.topStories!.value[indexPath.row]), callback: { (data : FDataSnapshot!) -> Void in
+                
+                if let title : AnyObject = data.value["title"] {
+                    cell.textLabel!.text = "\(indexPath.row) : \(title)"
+                }
+                else {
+                    cell.textLabel!.text = "\(indexPath.row)"
+                }
+            })
+        }
+        else {
+            cell.textLabel!.text = "\(indexPath.row)"
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var store = DataStore.sharedInstance
+        if let count = store.topStories?.childrenCount {
+            return Int(count)
+        }
+        else {
+            return 0
+        }
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
 
 }
 
